db3862ebf97dafb1107ee228c7babedd72814083
Abdelilah <lbardi.abdelilah@gmail.com>
2022-01-31 20:42:29 +0100

    Adds route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::match(['POST', 'PUT'], '/characters/{id}', \App\Http\Controllers\Characters\UpdateCharacterController::class)->name('characters.update');
